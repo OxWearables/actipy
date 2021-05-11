57a1c8d5faec091d51c4be375badd04aba560f84
Shing Chan <cshing.m@gmail.com>
2021-05-12 00:55:15 +0100

    rename device.py -> reader.py

--- pywear/__init__.py
+++ pywear/__init__.py
@@ -1,2 +1,2 @@
-from pywear.device import read_device
+from pywear.reader import read_device
 from pywear.processing import regularize_sample_rate, detect_nonwear, calibrate_gravity, get_stationary_indicator
