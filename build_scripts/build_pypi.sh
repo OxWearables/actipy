34a05e4bc1e0e6f69a146bc7c00341321a1856ee
Shing Chan <cshing.m@gmail.com>
2022-06-22 10:43:46 +0000

    add javac step in build script

--- build_scripts/build_pypi.sh
+++ build_scripts/build_pypi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+javac --release 8 actipy/*.java  # compile java stuff; use a low version e.g. 8 (1.8)
 python setup.py sdist bdist_wheel
 twine check dist/*
 # twine upload --repository-url https://test.pypi.org/legacy/ dist/*
