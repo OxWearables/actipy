c267c467098455668d0ba1bc06a57e71bcf4e3cf
Shing Chan <cshing.m@gmail.com>
2022-08-30 18:04:54 +0100

    build_conda.sh with -c conda-forge

--- build_scripts/build_conda.sh
+++ build_scripts/build_conda.sh
@@ -2,6 +2,6 @@
 
 # Be sure to have packaged for PyPI first
 conda skeleton pypi actipy --output-dir conda-recipe
-conda build conda-recipe/actipy
+conda build -c conda-forge conda-recipe/actipy
 # anaconda login
 # anaconda upload --user oxwear /path/to/package.tar.bz2
