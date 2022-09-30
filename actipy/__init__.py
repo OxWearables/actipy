f9212970b3ed6bfdcd46efb3544f87efcace990a
Shing Chan <cshing.m@gmail.com>
2022-09-30 12:33:05 +0000

    fix license

--- actipy/__init__.py
+++ actipy/__init__.py
@@ -2,6 +2,6 @@ name = "actipy"
 __version__ = "1.0.0"
 __author__ = "Shing Chan, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
-__license__ = "MIT"
+__license__ = "See LICENSE.md"
 
 from actipy.reader import read_device, process
