f43dc9f2446c1e31e2c66c5ce299e721f7045603
Shing Chan <cshing.m@gmail.com>
2022-10-01 07:43:24 +0000

    create install-dev.sh
    Simple developer installation script

---
+++ install-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# DEVELOPER INSTALLATION SCRIPT
+
+# You must have Java Development Kit (JDK) 8 (1.8). If higher (>8) then it must
+# support --release flag to pin down the version when compiling.
+# Always compile with 8 (1.8) to keep backward compatibility.
+# In conda, you can get a JDK version that supports --release flag:
+# conda install openjdk
+javac --version &&  # java version
+javac --release 8 actipy/*.java &&  # compile java files (using release 8)
+pip install -e .[dev,docs] &&  # install in edit mode
