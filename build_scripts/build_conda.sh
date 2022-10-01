afc7d4e7f6d3c467763753a7f7d5d069a658ab19
Shing Chan <cshing.m@gmail.com>
2022-10-01 02:50:01 +0000

    comment to hint install conda-build

--- build_scripts/build_conda.sh
+++ build_scripts/build_conda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Be sure to have packaged for PyPI first
+# conda install conda-build
 conda skeleton pypi actipy --output-dir conda-recipe
 conda build -c conda-forge conda-recipe/actipy
 # anaconda login
