b4974df9c76bac3283dd3fbcda0dd886b8b0c4c1
Shing Chan <cshing.m@gmail.com>
2023-02-17 15:53:49 +0000

    v2.0.1

--- actipy/__init__.py
+++ actipy/__init__.py
@@ -1,5 +1,5 @@
 name = "actipy"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Shing Chan, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
 __license__ = "See LICENSE.md"
