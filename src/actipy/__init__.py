d9a559f7496148d848387f615ad2cf028c43e0c8
Shing Chan <cshing.m@gmail.com>
2023-04-19 18:05:26 +0100

    update package metadata

--- src/actipy/__init__.py
+++ src/actipy/__init__.py
@@ -1,6 +1,8 @@
 name = "actipy"
 __author__ = "Shing Chan, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
+__maintainer__ = "Shing Chan"
+__maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
 __license__ = "See LICENSE.md"
 
 from actipy.reader import read_device, process
