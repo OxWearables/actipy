8a25ed449bdad8dab13d594489fc23bacc1ac5d2
Shing Chan <cshing.m@gmail.com>
2025-10-07 14:43:20 +0100

    fix: specify UTF-8 encoding when reading README in setup.py
    The README now contains emoji characters which cannot be decoded using
    Windows default cp1252 encoding. Explicitly specify UTF-8 encoding to
    fix GitHub Actions build failures on Windows.
    
    Fixes UnicodeDecodeError during pip install on Windows systems.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_string(string, rel_path="src/actipy/__init__.py"):
     else:
         raise RuntimeError(f"Unable to find {string}.")
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
